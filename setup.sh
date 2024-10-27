5a83f80c5ef567b72dd77047788340d7529c77e9
Abel Cabral de Arruda <jcabral@id.uff.br>
2024-10-27 03:44:31 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Defina a variável CREDENCIAL, voce deve passar o valor desse atributo no header de suas requisições {"credencial": "123456781"}
+# Defina a variável CREDENCIAL; passe o valor no header de suas requisições {"credencial": "123456781"}
 export CREDENCIAL=969369662
-echo -n "$CREDENCIAL" > credencial.txt
 
 # Define as variáveis de uso
 export VERSION="1.1.0"
@@ -17,10 +16,10 @@ sudo dnf install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Adiciona o usuário ec2-user ao grupo docker
+# Adiciona o usuário ec2-user ao grupo docker (será necessário logout e login do usuário)
 sudo usermod -aG docker ec2-user
 
-# Instala o Node.js, npm e unzip (substitua por uma versão específica de Node.js se necessário)
+# Instala o Node.js, npm e unzip
 sudo dnf install -y nodejs npm unzip
 
 # Instala o PM2 globalmente
@@ -34,39 +33,41 @@ sudo swapon /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 # Muda para o diretório do usuário padrão (ec2-user)
-cd /home/ec2-user || exit
+cd "$HOME" || exit
 
-# Salvado senha de validacao de acesso
-echo "export CHAVE_DE_ACESSO=\"$CREDENCIAL\"" >> ~/.bashrc
-source ~/.bashrc
+# Salvando senha de validação de acesso de forma segura
+echo -n "$CREDENCIAL" > credencial.txt
 
 # Baixa o arquivo zip da release
 curl -L -o "${APP_NAME}.zip" "https://github.com/abel-cabral/abel-dockManager/releases/download/v${VERSION}/v${VERSION}.zip"
+if [ $? -ne 0 ]; then
+    echo "Erro ao baixar o arquivo da release. Verifique a URL."
+    exit 1
+fi
 
 # Cria a pasta do aplicativo
-mkdir "${APP_NAME}"
+mkdir -p "${APP_NAME}"
 
 # Extrai o conteúdo do zip para a pasta do aplicativo
-unzip "${APP_NAME}.zip" -d "${APP_NAME}"
+unzip -o "${APP_NAME}.zip" -d "${APP_NAME}"
+if [ $? -ne 0 ]; then
+    echo "Erro ao extrair o arquivo zip."
+    exit 1
+fi
 
-# Deleta o zip que foi extraido
-rm -rf "${APP_NAME}.zip"
+# Deleta o zip que foi extraído
+rm -f "${APP_NAME}.zip"
 
 # Inicia a aplicação com PM2, limitando o uso de memória a 70 MB
-sudo pm2 start "/home/ec2-user/${APP_NAME}/app.js" --name "${APP_NAME}" --max-memory-restart 70M
-sudo chown ec2-user:ec2-user /home/ec2-user/.pm2/rpc.sock /home/ec2-user/.pm2/pub.sock
+pm2 start "$HOME/${APP_NAME}/app.js" --name "${APP_NAME}" --max-memory-restart 70M
+sudo chown ec2-user:ec2-user $HOME/.pm2/rpc.sock $HOME/.pm2/pub.sock
 
 # Configura o PM2 para iniciar no boot do sistema
-sudo pm2 startup systemd
-
-# Salva a configuração atual do PM2
-sudo pm2 save
+pm2 startup systemd -u ec2-user --hp "$HOME"
+pm2 save
 
 # Exibe o status do PM2
-sudo pm2 status
-
-# Reinicia a sessão do usuário para garantir que o grupo docker seja atualizado
-newgrp docker
+pm2 status
 
 # Inicializa o Docker Swarm
 docker swarm init

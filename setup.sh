3b9915c7e48f3203d3f9c2c6bbd5843c888fb853
Abel Cabral de Arruda <jcabral@id.uff.br>
2024-10-27 13:58:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 export CREDENCIAL=969369662
 
 # Define as variáveis de uso
-export VERSION="1.1.0"
 export APP_NAME="aws-app-hub-api"
 export HOME=/home/ec2-user
 
@@ -38,29 +37,53 @@ cd "$HOME" || exit
 # Salvando senha de validação de acesso de forma segura
 echo -n "$CREDENCIAL" > credencial.txt
 
-# Baixa o arquivo zip da release
-curl -L -o "${APP_NAME}.zip" "https://github.com/abel-cabral/aws-app-hub-api/releases/download/v${VERSION}/v${VERSION}.zip"
-if [ $? -ne 0 ]; then
-    echo "Erro ao baixar o arquivo da release. Verifique a URL."
+# Obtém o link do arquivo zip da última release via API do GitHub
+echo "Obtendo o link da última versão da release via API do GitHub..."
+DOWNLOAD_URL=$(curl -s https://api.github.com/repos/abel-cabral/${APP_NAME}/releases/latest | grep "browser_download_url.*zip" | cut -d '"' -f 4)
+
+if [ -z "$DOWNLOAD_URL" ]; then
+    echo "Erro ao obter o link de download da última release."
     exit 1
 fi
 
-# Cria a pasta do aplicativo
+# Baixa o arquivo zip da última release
+curl -L -o "${APP_NAME}.zip" "$DOWNLOAD_URL"
+if [ $? -ne 0 ] || [ ! -f "${APP_NAME}.zip" ]; then
+    echo "Erro ao baixar o arquivo da release ou o arquivo está bloqueado."
+    exit 1
+fi
+
+# Define permissão de leitura para o arquivo zip
+chmod 644 "${APP_NAME}.zip"
+
+# Verifica o arquivo baixado
+file "${APP_NAME}.zip" | grep -q "Zip archive data"
+if [ $? -ne 0 ]; then
+    echo "O arquivo baixado não é um arquivo zip válido."
+    exit 1
+fi
+
+# Cria a pasta do aplicativo se não existir
 mkdir -p "${APP_NAME}"
 
 # Extrai o conteúdo do zip para a pasta do aplicativo
+echo "Extraindo o conteúdo do arquivo zip..."
 unzip -o "${APP_NAME}.zip" -d "${APP_NAME}"
 if [ $? -ne 0 ]; then
-    echo "Erro ao extrair o arquivo zip."
+    echo "Erro ao extrair o arquivo zip. Verifique a integridade do arquivo."
     exit 1
 fi
 
 # Deleta o zip que foi extraído
 rm -f "${APP_NAME}.zip"
 
+# Verifica e corrige permissões da pasta .pm2
+echo "Corrigindo permissões da pasta .pm2..."
+sudo chown -R ec2-user:ec2-user /home/ec2-user/.pm2
+
 # Inicia a aplicação com PM2, limitando o uso de memória a 70 MB
 pm2 start "$HOME/${APP_NAME}/app.js" --name "${APP_NAME}" --max-memory-restart 70M
-sudo chown ec2-user:ec2-user $HOME/.pm2/rpc.sock $HOME/.pm2/pub.sock
+sudo chown ec2-user:ec2-user "$HOME/.pm2/rpc.sock" "$HOME/.pm2/pub.sock"
 
 # Configura o PM2 para iniciar no boot do sistema
 pm2 startup systemd -u ec2-user --hp "$HOME"
